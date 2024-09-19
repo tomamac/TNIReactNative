7588ab11cb3bd3ef9ccbd6df1dd44bc9c87467ad
tomamac <mek777@hotmail.co.th>
2024-09-20 00:47:01 +0700

    Catch up 3 (Detail Screen)

--- MyFirstProject/screens/DetailScreen.tsx
+++ MyFirstProject/screens/DetailScreen.tsx
@@ -4,21 +4,24 @@ import {
   ActivityIndicator,
   FlatList,
   ListRenderItem,
+  StyleSheet,
+  Dimensions,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import {
   useNavigation,
   useRoute,
   useFocusEffect,
 } from "@react-navigation/native";
 import { findProductbyId } from "../services/product-service";
-import { ListItem, Avatar } from "@rneui/themed";
+import { ListItem, Tile } from "@rneui/themed";
 
 const DetailScreen = (): React.JSX.Element => {
   const route = useRoute<any>();
   const navigation = useNavigation<any>();
 
-  const [detail, setDetail] = React.useState<any>([]);
+  const [detail, setDetail] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -29,10 +32,13 @@ const DetailScreen = (): React.JSX.Element => {
   const getProductbyId = async () => {
     try {
       const response = await findProductbyId(route.params.id);
+      setLoading(true);
       //console.log(response.data.data);
       setDetail(response.data.data);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
   useFocusEffect(
@@ -43,9 +49,19 @@ const DetailScreen = (): React.JSX.Element => {
   const _renderItem: ListRenderItem<any> = ({ item }) => {
     return (
       <>
-        <ListItem.Content>
-          <ListItem.Title>{item.ch_title}</ListItem.Title>
-        </ListItem.Content>
+        <Tile
+          imageSrc={{
+            uri: "https://www.mediastorehouse.com/p/191/sunset-porthmeor-beach-st-ives-cornwall-11702500.jpg.webp",
+          }}
+          title={item.ch_title}
+          titleStyle={styles.titleStyle}
+          containerStyle={styles.tileContainer}
+          caption={item.ch_dateadd}
+          captionStyle={styles.captionStyle}
+          featured
+          activeOpacity={0.9}
+          width={Dimensions.get("screen").width - 20} // ลบขอบเล็กน้อยทั้งสองข้าง
+        />
       </>
     );
   };
@@ -56,9 +72,42 @@ const DetailScreen = (): React.JSX.Element => {
         data={detail}
         keyExtractor={(item: any) => item.ch_id.toString()}
         renderItem={_renderItem}
+        onRefresh={() => {
+          getProductbyId();
+        }}
+        refreshing={loading}
+        contentContainerStyle={styles.listContent}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f0f0f0", // สีพื้นหลัง
+  },
+  listContent: {
+    paddingHorizontal: 10, // การเว้นวรรคขอบซ้ายและขวาเท่ากัน
+  },
+  titleStyle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "blue", // สีขาวสำหรับชื่อ
+  },
+  tileContainer: {
+    borderRadius: 10, //กำหนดให้มุมของ container มีความโค้งมน
+    overflow: "hidden", //กำหนดให้เนื้อหาที่อาจล้นออกจากขอบของ container ไม่แสดงผล
+    marginBottom: 10, //กำหนดระยะห่างจากขอบล่างของ container ไปยัง element ต่อไปที่อยู่ด้านล่าง
+    elevation: 5, // เงาสำหรับ Android
+    shadowOffset: { width: 0, height: 2 }, //กำหนดตำแหน่งของเงาที่แสดงผล
+    shadowOpacity: 0.25, //กำหนดระดับความโปร่งแสงของเงา
+    shadowRadius: 3.84, //กำหนดรัศมีของการกระจายตัวของเงา
+    opacity: 0.5, // ความโปร่งใสของภาพ , ค่าน้อยจะโปร่งใสมาก
+  },
+  captionStyle: {
+    fontSize: 14,
+    color: "blue", // สีขาวสำหรับวันที่
+  },
+});
 
 export default DetailScreen;
